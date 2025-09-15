82f5addb1397cd529904409553ff5fd2830cd2de
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-15 16:09:34 -0600

    en el main

--- src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
+++ src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
@@ -2,10 +2,12 @@ package com.panquesitos.recursos_humanos.principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.panquesitos.recursos_humanos.repository")
+@EntityScan("com.panquesitos.recursos_humanos.model") // paquete de tus entidades
+@EnableJpaRepositories("com.panquesitos.recursos_humanos.repository") // paquete de repositorios
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
