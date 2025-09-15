c384ae8aefe8c934f12daa6fb48c96161ed4d28f
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-15 15:30:21 -0600

    Funcionalidad de controller y conexion de base de datos

--- src/main/java/com/panquesitos/recursos_humanos/controllador/ControllerPorfesor.java
+++ src/main/java/com/panquesitos/recursos_humanos/controllador/ControllerPorfesor.java
@@ -1,4 +1,29 @@
 package com.panquesitos.recursos_humanos.controllador;
 
+import com.panquesitos.recursos_humanos.model.Profesor;
+import com.panquesitos.recursos_humanos.service.ProfesorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
 public class ControllerPorfesor {
+    @Autowired
+    private ProfesorService profesorService;
+
+    // Registrar profesor
+    @PostMapping
+    public ResponseEntity<Profesor> registrar(@RequestBody Profesor profesor) {
+        Profesor guardado = profesorService.registrarProfesor(profesor);
+        return ResponseEntity.ok(guardado);
+    }
+
+    // Login básico (solo busca por correo)
+    @GetMapping("/login")
+    public ResponseEntity<Profesor> login(@RequestParam String correo) {
+        Optional<Profesor> profesor = profesorService.login(correo);
+        return profesor.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
