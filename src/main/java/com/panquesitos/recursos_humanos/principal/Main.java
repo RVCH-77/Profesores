ad0c803547f4f348444781f2879a51e79b7133e9
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-15 16:08:16 -0600

    cambios en el repositorio y en el application.propierties

--- src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
+++ src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
@@ -1,4 +1,13 @@
 package com.panquesitos.recursos_humanos.principal;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.panquesitos.recursos_humanos.repository")
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
