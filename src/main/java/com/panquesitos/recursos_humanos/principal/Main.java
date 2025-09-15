391659cfeb92535d1c785edba2f0c12efaee5b16
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-15 17:17:20 -0600

    cambios en el main

--- src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
+++ src/main/java/com/panquesitos/recursos_humanos/principal/Main.java
@@ -1,11 +1,17 @@
 package com.panquesitos.recursos_humanos.principal;
 
+import  com.panquesitos.recursos_humanos.model.Profesor;
+import  com.panquesitos.recursos_humanos.repository.ProfesorRepository;
+import  com.panquesitos.recursos_humanos.service.ProfesorService;
+import   com.panquesitos.recursos_humanos.controllador.ControllerPorfesor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.panquesitos.recursos_humanos")
 @EntityScan("com.panquesitos.recursos_humanos.model") // paquete de tus entidades
 @EnableJpaRepositories("com.panquesitos.recursos_humanos.repository") // paquete de repositorios
 public class Main {
