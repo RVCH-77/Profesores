5b46f54d51a8c81b8ea2a05025f90faa09014059
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-16 13:09:58 -0600

    busqueda por materia

--- src/main/java/com/panquesitos/recursos_humanos/repository/MateriaRepository.java
+++ src/main/java/com/panquesitos/recursos_humanos/repository/MateriaRepository.java
@@ -4,4 +4,7 @@ import com.panquesitos.recursos_humanos.model.Materia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MateriaRepository extends JpaRepository<Materia, Integer >{
+
+    // Buscar por nombre
+    Materia findByNombre(String nombre);
 }
