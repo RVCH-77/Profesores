f609e4453cce1163596907f05c06a3bda3514d31
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-16 13:04:44 -0600

    añadi los atributos de materia

--- src/main/java/com/panquesitos/recursos_humanos/model/Materia.java
+++ src/main/java/com/panquesitos/recursos_humanos/model/Materia.java
@@ -1,4 +1,20 @@
 package com.panquesitos.recursos_humanos.model;
-
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name = "materias")
+@Getter
+@Setter
 public class Materia {
-}
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer idMateria;
+
+        @Column(nullable = false, unique = true)
+        private String nombre;
+    }
+
+
