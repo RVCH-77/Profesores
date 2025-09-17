6321a90cd400f0c939406712ef6adec6c65da005
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-17 11:03:32 -0600

    datos en el repositoryAdministrador

---
+++ src/main/java/com/panquesitos/recursos_humanos/repository/AdministradoresRepository.java
@@ -0,0 +1,21 @@
+package com.panquesitos.recursos_humanos.repository;
+import com.panquesitos.recursos_humanos.model.Administradores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradoresRepository extends JpaRepository <Administradores, Long> {
+
+    // Login
+    Administradores findByCorreoAndContrasena(String correo, String contrasena);
+
+    // Buscar por correo
+    Administradores findByCorreo(String correo);
+
+    // Buscar por id
+    Administradores findById(long id);
+
+    // Buscar por nombre
+    Administradores findByNombre(String nombre);
+
+
+
+}
