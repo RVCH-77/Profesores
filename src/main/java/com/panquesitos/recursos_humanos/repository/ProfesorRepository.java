426a9ea48d6638553819ca16c9279e002347b635
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-09-20 18:59:29 -0600

    Elimine todo relacionado con materia

--- src/main/java/com/panquesitos/recursos_humanos/repository/ProfesorRepository.java
+++ src/main/java/com/panquesitos/recursos_humanos/repository/ProfesorRepository.java
@@ -1,6 +1,5 @@
 package com.panquesitos.recursos_humanos.repository;
 
-import com.panquesitos.recursos_humanos.model.Materia;
 import com.panquesitos.recursos_humanos.model.Profesor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
